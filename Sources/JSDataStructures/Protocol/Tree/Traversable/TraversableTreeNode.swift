0aa524676c6b5748f9256273acd4975f0259c449
Jonathan Sack <jonathan.sack91@gmail.com>
2022-02-02 01:49:20 +0200

    Add filter and map methods to tree traversal methods

--- Sources/JSDataStructures/Protocol/Tree/Traversable/TraversableTreeNode.swift
+++ Sources/JSDataStructures/Protocol/Tree/Traversable/TraversableTreeNode.swift
@@ -8,28 +8,74 @@
 
 // MARK: - Protocol
 public protocol TraversableTreeNode: TreeNodeProtocol where Element: TraversableTreeNode {
-    func forEachDepthFirst(completion: (Element) -> Void)
-    func forEachLevelOrder(completion: (Element) -> Void)
+    @inlinable func forEachDepthFirst(completion: (Element) throws -> Void) rethrows
+    @inlinable func forEachLevelOrder(completion: (Element) throws -> Void) rethrows
+    @inlinable func filterDepthFirst(_ isIncluded: (Element) throws -> Bool) rethrows -> [Element]
+    @inlinable func filterLevelOrder(_ isIncluded: (Element) throws -> Bool) rethrows -> [Element]
+    @inlinable func mapDepthFirst<T>(_ transform: (Element) throws -> T) rethrows -> [T]
+    @inlinable func mapLevelOrder<T>(_ transform: (Element) throws -> T) rethrows -> [T]
 }
 
 // MARK: - EXT. Traversal Methods
 public extension TraversableTreeNode where Self == Element {
 
-    // For Each Depth First (aka pre-order)
-    func forEachDepthFirst(completion: (Element) -> Void) {
-        completion(self)
-        children.forEach { $0.forEachDepthFirst(completion: completion) }
+    // MARK: - Depth First (Pre-Order)
+    // For Each
+    @inlinable func forEachDepthFirst(completion: (Element) throws -> Void) rethrows {
+        try completion(self)
+        try children.forEach { try $0.forEachDepthFirst(completion: completion) }
     }
 
-    // For Each Level Order
-    func forEachLevelOrder(completion: (Element) -> Void) {
+    // Filter
+    @inlinable func filterDepthFirst(_ isIncluded: (Element) throws -> Bool) rethrows -> [Element] {
+        var result: [Element] = []
+        try forEachDepthFirst {
+            guard try isIncluded($0) else { return }
+            result.append($0)
+        }
+        return result
+    }
+
+    // Map
+    @inlinable func mapDepthFirst<T>(_ transform: (Element) throws -> T) rethrows -> [T] {
+        var result: [T] = []
+        try forEachDepthFirst {
+            let transformedElement = try transform($0)
+            result.append(transformedElement)
+        }
+        return result
+    }
+
+    // MARK: - Level Order
+    // For Each
+    @inlinable func forEachLevelOrder(completion: (Element) throws -> Void) rethrows {
 
         var queue = Queue<Element>()
         queue.enqueue(self)
 
         while let node = queue.dequeue() {
-            completion(node)
+            try completion(node)
             node.children.forEach { queue.enqueue($0) }
         }
+    }
+    
+    // Filter
+    @inlinable func filterLevelOrder(_ isIncluded: (Element) throws -> Bool) rethrows -> [Element] {
+        var result: [Element] = []
+        try forEachLevelOrder {
+            guard try isIncluded($0) else { return }
+            result.append($0)
+        }
+        return result
+    }
+    
+    // Map
+    @inlinable func mapLevelOrder<T>(_ transform: (Element) throws -> T) rethrows -> [T] {
+        var result: [T] = []
+        try forEachLevelOrder {
+            let transformedElement = try transform($0)
+            result.append(transformedElement)
+        }
+        return result
     }
 }
--- Tests/JSDataStructuresTests/TreeNodeTests.swift
+++ Tests/JSDataStructuresTests/TreeNodeTests.swift
@@ -11,6 +11,7 @@ import XCTest
 
 class TreeNodeTests: XCTestCase {
 
+    // MARK: - Properties
     var root: TreeNode<Int>!
     var one: TreeNode<Int>!
     var two: TreeNode<Int>!
@@ -20,6 +21,7 @@ class TreeNodeTests: XCTestCase {
     var ten: TreeNode<Int>!
     var twelve: TreeNode<Int>!
     
+    // MARK: - Set Up
     override func setUpWithError() throws {
         //       GRAPH
         //
@@ -57,6 +59,7 @@ class TreeNodeTests: XCTestCase {
         three.insert(ten)
     }
 
+    // MARK: - Tear Down
     override func tearDownWithError() throws {
         root = nil
         one = nil
@@ -68,14 +71,13 @@ class TreeNodeTests: XCTestCase {
         twelve = nil
     }
 
+    // MARK: - Traversal Tests
     func testTreeNode_WhenTraversingDepthFirst_ShouldReturnCorrectDepthFirstOrder() {
         // Arrange
         let validOrder: [Int] = [4, 1, 2, 7, 9, 12, 3, 10]
-        
         // Act
         var depthFirstOrder: [Int] = []
         root.forEachDepthFirst { depthFirstOrder.append($0.value) }
-        
         // Assert
         XCTAssertEqual(depthFirstOrder, validOrder, "Depth first traversal returned an invalid order")
     }
@@ -83,44 +85,86 @@ class TreeNodeTests: XCTestCase {
     func testTreeNode_WhenTraversingLevelOrder_ShouldReturnCorrectLevelOrder() {
         // Arrange
         let validOrder: [Int] = [4, 1, 7, 9, 2, 12, 3, 10]
-        
         // Act
         var levelOrder: [Int] = []
         root.forEachLevelOrder { levelOrder.append($0.value) }
-        
         // Assert
         XCTAssertEqual(levelOrder, validOrder, "Depth first traversal returned an invalid order")
     }
 
+    // MARK: - Filtering Tests
+    func testTreeNode_WhenFilteringDepthFirst_ShouldReturnFilteredDepthFirstOrder() {
+        // Arrange
+        let validOrder1: [Int] = [9, 12, 10]
+        let validOrder2: [Int] = [2, 7, 12, 10]
+        // Act
+        let depthFirstOrder1: [TreeNode<Int>] = root.filterDepthFirst { $0.value > 8 }
+        let depthFirstOrder2: [TreeNode<Int>] = root.filterDepthFirst { $0.isLeaf }
+        // Assert
+        precondition(validOrder1.count == depthFirstOrder1.count)
+        precondition(validOrder2.count == depthFirstOrder2.count)
+        validOrder1.indices.forEach { XCTAssertEqual(validOrder1[$0], depthFirstOrder1[$0].value) }
+        validOrder2.indices.forEach { XCTAssertEqual(validOrder2[$0], depthFirstOrder2[$0].value) }
+    }
+    
+    func testTreeNode_WhenFilteringLevelOrder_ShouldReturnFilteredLevelOrder() {
+        // Arrange
+        let validOrder1: [Int] = [4, 1, 7, 2, 3]
+        let validOrder2: [Int] = [4, 1, 9, 3]
+        // Act
+        let levelOrder1: [TreeNode<Int>] = root.filterLevelOrder { $0.value < 8 }
+        let levelOrder2: [TreeNode<Int>] = root.filterLevelOrder { !$0.isLeaf }
+        // Assert
+        precondition(validOrder1.count == levelOrder1.count)
+        precondition(validOrder2.count == levelOrder2.count)
+        validOrder1.indices.forEach { XCTAssertEqual(validOrder1[$0], levelOrder1[$0].value) }
+        validOrder2.indices.forEach { XCTAssertEqual(validOrder2[$0], levelOrder2[$0].value) }
+    }
+
+    // MARK: - Map Tests
+    func testTreeNode_WhenMappingDepthFirst_ShouldReturnCorrectMappedDepthFirstOrder() {
+        // Arrange
+        let validOrder: [Int] = [40, 10, 20, 70, 90, 120, 30, 100]
+        // Act
+        let depthFirstOrder: [Int] = root.mapDepthFirst { $0.value * 10 }
+        // Assert
+        XCTAssertEqual(depthFirstOrder, validOrder, "Map depth first traversal should returned mapped values in depth first order")
+    }
+
+    func testTreeNode_WhenMappingLevelOrder_ShouldReturnCorrectMappedLevelOrder() {
+        // Arrange
+        let validOrder: [Int] = [400, 100, 700, 900, 200, 1200, 300, 1000]
+        // Act
+        let levelOrder: [Int] = root.mapLevelOrder { $0.value * 100 }
+        // Assert
+        XCTAssertEqual(levelOrder, validOrder, "Map level order traversal should returned mapped values in level order")
+    }
+
+    // MARK: - Height Tests
     func testTreeNode_WhenGettingHeightOfLeafNode_ShouldReturnZero() {
         // Arrange
         assert(two.isLeaf)
-        
         // Act
         let height = two.height
-        
         // Assert
         XCTAssertEqual(height, 0, "Height of leaf node should be 0 but it is not")
     }
     
-    func testTreeNode_WhenGettingHeightOfNonLeafNode_ShouldReturnCorrectDepthValue() {
+    func testTreeNode_WhenGettingHeightOfNonLeafNode_ShouldReturnCorrectHeightValue() {
         // Arrange
         assert(!root.isLeaf)
-        
         // Act
         let height = root.height
-
         // Assert
         XCTAssertEqual(height, 3, "Depth of root node should be 3 but it is not")
     }
 
+    // MARK: - Depth Tests
     func testTreeNode_WhenGettingDepthOfRootNode_ShouldReturnZero() {
         // Arrange
         assert(root.parent == nil)
-        
         // Act
         let depth = root.depth
-        
         // Assert
         XCTAssertEqual(depth, 0, "Depth of root node should be 0 but it is not")
     }
@@ -128,10 +172,8 @@ class TreeNodeTests: XCTestCase {
     func testTreeNode_WhenGettingHeightOfNonRootNode_ShouldReturnCorrectDepthValue() {
         // Arrange
         assert(twelve.parent != nil)
-        
         // Act
         let depth = twelve.depth
-
         // Assert
         XCTAssertEqual(depth, 2, "Depth of node twelve should be 2 but it is not")
     }
